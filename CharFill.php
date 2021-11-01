aca3b0d93402a5aad826eb9677f0a7b8420fdfac
Nenad Stevic <Nenad.Stevic@bene-edu.ch>
2021-11-01 15:29:44 +0100

    added DrawTable function

--- CharFill.php
+++ CharFill.php
@@ -1,8 +1,10 @@
 <?php
     include 'CharRand.php';
-    /*
-    **Function that fills a table array with random symbols using charRand() and returns it. Takes total number of rows and columns as parameters
-    */
+
+    /***
+    ****Function that fills an array with random symbols using charRand() and returns it. 
+    ****Takes total number of rows and columns as parameters
+    ***/
     function createTableArray(int $rows, int $cols){
         $table = array();
         
@@ -11,5 +13,61 @@
             $table[$i] = charRand();
         }
         return $table;
+    }
+
+    /***
+    ***/
+    function drawAndFillTable(){
+        $rows = 15;
+        $cols = 48;
+        $header_footer = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        $passwordarray = createTableArray($rows, $cols);
+        $iterator = 0;
+        $char_amount = 1;
+
+        echo "<table>";
+            echo "<th></th>";
+
+        for($i = 1; $i <= $cols; $i++){
+            echo "<th>";
+            for($ii = 1; $ii <= $char_amount; $ii++){
+                echo $header_footer[$iterator];
+            }
+            echo "</th>";
+            $iterator++;
+            if($iterator == 25){
+                $iterator = 0;
+                $char_amount++;
+            }
+        }
+
+        $iterator = 0;
+
+        for($line = 1; $line <= $rows; $line++){
+            echo "<tr>";
+            for($cell = 0; $cell <= $cols; $cell++){
+                if($cell == 0) echo "<td>" . $line . "</td>";
+                else{
+                    echo "<td>" . $passwordarray[$iterator] . "</td>";
+                $iterator++;
+                }
+            }
+            echo "</tr>";
+        }
+
+        $iterator = 0;
+            echo "<tfoot>";
+                echo "<tr>";
+
+        for($i = 0; $i <= $cols; $i++){
+            if($i == 0) echo "<td></td>";
+            else {echo "<td>" . $header_footer[$iterator] . "</td>";
+                $iterator++;
+                if($iterator == 25) $iterator = 0;
+            }
+        }
+                echo "</tr>";
+            echo "</tfoot>";
+        echo "</table>";
     }
 ?>
