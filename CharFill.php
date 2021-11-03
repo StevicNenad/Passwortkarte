51fceb9360f2604a6a3bf91efd63dc3ee0bb2c47
Nenad Stevic <Nenad.Stevic@bene-edu.ch>
2021-11-03 10:53:55 +0100

    Clean Coded the shit out of CharFill and CharRand

--- CharFill.php
+++ CharFill.php
@@ -10,21 +10,27 @@
 
         echo ($label == "header") ? "<thead>" : "<tfoot><tr>";
 
+        //Loop that creates header and footer labels. Checks if $labels is "header" or "footer" and echoes the proper html command
+        //Checks if letter "Z" has been reached and adds an additonal letter: ex. "Z" and then "AA", "ZZ" and then "AAA"
         for($i = 0; $i < $cols; $i++){
             if($i == 0) echo ($label == "header") ? "<th></th>" : "<td></td>";
             echo ($label == "header") ? "<th>" : "<td>";
+
             if($third_char > 0) echo $head_foot_labeling[$third_char - 1];
             if($second_char > 0) echo $head_foot_labeling[$second_char - 1];
+
             echo $head_foot_labeling[$first_char];
             echo ($label == "header") ? "</th>" : "</td>";
 
             $first_char++;
 
+            //Adds a third letter Ex. "AAA"
             if($second_char == 26 && $first_char == 26){
                 $third_char++;
                 $second_char = 1;
                 $first_char = 0;
             }
+            //Adds a second letter Ex. "AA"
             else if($first_char == 26){
                 $second_char++;
                 $first_char = 0;
@@ -39,16 +45,15 @@
     function drawAndFillTable($rows, $cols){
         $passwordarray = createTableArray($rows, $cols);
         $iterator = 0;
-        $char_amount = 0;
 
         echo "<table>";
-
         generateHeaderFooter("header", $cols);
 
+        //Nested for-loop that takes total number of columns and rows as arguments and starts filling the table
         for($line = 1; $line <= $rows; $line++){
             echo "<tr>";
             for($cell = 0; $cell <= $cols; $cell++){
-                if($cell == 0) echo "<td>" . $line . "</td>";
+                if($cell == 0) echo "<td class="row_number">" . $line . "</td>";
                 else{
                     echo "<td>" . $passwordarray[$iterator] . "</td>";
                 $iterator++;
@@ -56,9 +61,7 @@
             }
             echo "</tr>";
         }
-
         generateHeaderFooter("footer", $cols);
-
         echo "</table>";
     }
 ?>
